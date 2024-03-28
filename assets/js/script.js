70681bc84b331e8a1f83fdc817af358ce386e350
Seema-476 <simmybishnoi29085@gmail.com>
2024-03-28 14:14:39 +0530

    section 2 done

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,82 @@ menuicon.addEventListener("click", function () {
     menulist.classList.toggle("show")
     document.body.classList.toggle("overflowhidden")
 })
+
+$('.responsive').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    // autoplay: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 815,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 778,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 320,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
+});
+$('.responsive2').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    // autoplay: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 815,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 778,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 320,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
+});
