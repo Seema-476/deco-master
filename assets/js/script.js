734948ee8387f481b5a9f34d2c527f4d939eac6f
Seema-476 <simmybishnoi29085@gmail.com>
2024-04-03 12:03:56 +0530

    added ellipse btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,18 @@ menuicon.addEventListener("click", function () {
     document.body.classList.toggle("overflowhidden")
     document.querySelector(".menuicon").classList.toggle("cross");
 })
+const topBtn = document.querySelector(".topBtn")
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 60) {
+        topBtn.classList.add("show")
+    } else {
+        topBtn.classList.remove("show");
+    }
+})
+topBtn.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+})
 
 $('.responsive').slick({
     dots: false,
